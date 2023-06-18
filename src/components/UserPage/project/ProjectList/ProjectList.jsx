03a42ecea609b9f305aef46112938a287105b3a4
leech-oeo <fairprice81000@gmail.com>
2023-06-18 14:46:04 +0200

    update project list

--- src/components/UserPage/project/ProjectList/ProjectList.jsx
+++ src/components/UserPage/project/ProjectList/ProjectList.jsx
@@ -3,193 +3,38 @@ import {
   ActiveProjectContainer,
   OldProjectContainer,
   ProjectItem,
+  LoaderContainer,
+  Loader,
 } from "./style";
 import { useNavigate } from "react-router-dom";
 import UserNavBar from "../../NavBar/UserNavBar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 const ProjectList = () => {
   const navigate = useNavigate();
   const [showOldProject, setShowOldProject] = useState(false);
-  const templist = [
-    {
-      projectName: "cool project1",
-      isActive: true,
-      products: [
-        {
-          location: { lat: "52.52354266184729", lon: "13.402872545835251" },
-          area: "2",
-          orientation: "East",
-          tilt: "2",
-          type: "3",
-        },
-        {
-          location: { lat: "29.790997667390602", lon: "-95.42082544600602" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-        {
-          location: { lat: "10.354327612932527", lon: "8.9098909746236" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-        {
-          location: { lat: "-40.05223471309356", lon: "175.7040239802963" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-      ],
-    },
-    {
-      projectName: "cool project2",
-      isActive: true,
-      products: [
-        {
-          location: { lat: "52.52354266184729", lon: "13.402872545835251" },
-          area: "2",
-          orientation: "East",
-          tilt: "2",
-          type: "3",
-        },
-        {
-          location: { lat: "29.790997667390602", lon: "-95.42082544600602" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-        {
-          location: { lat: "10.354327612932527", lon: "8.9098909746236" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-        {
-          location: { lat: "-40.05223471309356", lon: "175.7040239802963" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-      ],
-    },
-    {
-      projectName: "cool project3",
-      isActive: true,
-      products: [
-        {
-          location: { lat: "52.52354266184729", lon: "13.402872545835251" },
-          area: "2",
-          orientation: "East",
-          tilt: "2",
-          type: "3",
-        },
-        {
-          location: { lat: "29.790997667390602", lon: "-95.42082544600602" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-        {
-          location: { lat: "10.354327612932527", lon: "8.9098909746236" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-        {
-          location: { lat: "-40.05223471309356", lon: "175.7040239802963" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-      ],
-    },
-    {
-      projectName: "cool project4",
-      isActive: true,
-      products: [
-        {
-          location: { lat: "52.52354266184729", lon: "13.402872545835251" },
-          area: "2",
-          orientation: "East",
-          tilt: "2",
-          type: "3",
-        },
-        {
-          location: { lat: "29.790997667390602", lon: "-95.42082544600602" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-        {
-          location: { lat: "10.354327612932527", lon: "8.9098909746236" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-        {
-          location: { lat: "-40.05223471309356", lon: "175.7040239802963" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-      ],
-    },
+  const [activeProjectList, setActivateProjectList] = useState([]);
+  const [oldProjectList, setOldProjectList] = useState([]);
+  const [isFetched, setIsFetched] = useState(false);
 
-    {
-      projectName: "cool old  project1",
-      isActive: false,
-      products: [
-        {
-          location: { lat: "52.52354266184729", lon: "13.402872545835251" },
-          area: "2",
-          orientation: "East",
-          tilt: "2",
-          type: "3",
-        },
-        {
-          location: { lat: "29.790997667390602", lon: "-95.42082544600602" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-        {
-          location: { lat: "10.354327612932527", lon: "8.9098909746236" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-        {
-          location: { lat: "-40.05223471309356", lon: "175.7040239802963" },
-          area: "1",
-          orientation: "East",
-          tilt: "3",
-          type: "4",
-        },
-      ],
-    },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/productList2");
+        if (response) {
+          setIsFetched(true);
+        }
+        setActivateProjectList(
+          response.data.filter((project) => project.isActive === true)
+        );
+        setOldProjectList(
+          response.data.filter((project) => project.isActive === false)
+        );
+      } catch (error) {}
+    };
+    fetchData();
+  }, []);
 
-  const [activeProjectList, setActivateProjectList] = useState(
-    templist.filter((project) => project.isActive === true)
-  );
-  const [oldProjectList, setOldProjectList] = useState(
-    templist.filter((project) => project.isActive === false)
-  );
   const handleGetReport = (project, index) => {
     console.log(project);
     const updatedActiveProductList = [...activeProjectList];
@@ -204,23 +49,64 @@ const ProjectList = () => {
     <div>
       <UserNavBar />
       <h1>Project list</h1>
+      {!isFetched && (
+        <LoaderContainer>
+          <Loader />
+        </LoaderContainer>
+      )}
+      {activeProjectList.length !== 0 && (
+        <>
+          <button
+            onClick={() => {
+              navigate("/user/new-project");
+            }}
+          >
+            create new project
+          </button>
+          <button onClick={() => setShowOldProject(!showOldProject)}>
+            {showOldProject ? "hide old project" : "show old project"}
+          </button>
+        </>
+      )}
+      {isFetched && activeProjectList.length === 0 ? (
+        <div>
+          <h2>create your new project</h2>{" "}
+          <button
+            onClick={() => {
+              navigate("/user/new-project");
+            }}
+          >
+            create new project
+          </button>
+        </div>
+      ) : (
+        <ProjectListContainer>
+          {showOldProject && (
+            <OldProjectContainer>
+              {oldProjectList.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <h3>{item.projectName}</h3>
+                    <p> number of products: {item.products.length}</p>
 
-      <button
-        onClick={() => {
-          navigate("/user/new-project");
-        }}
-      >
-        create new project
-      </button>
-      <button onClick={() => setShowOldProject(!showOldProject)}>
-        {showOldProject ? "hide old project" : "show old project"}
-      </button>
-      <ProjectListContainer>
-        {showOldProject && (
-          <OldProjectContainer>
-            {oldProjectList.map((item, index) => {
+                    <button
+                      onClick={() => {
+                        navigate("/user/project-detail", {
+                          state: { projectInfo: item },
+                        });
+                      }}
+                    >
+                      show report
+                    </button>
+                  </div>
+                );
+              })}
+            </OldProjectContainer>
+          )}
+          <ActiveProjectContainer>
+            {activeProjectList.map((item, index) => {
               return (
-                <div key={index}>
+                <ProjectItem key={index}>
                   <h3>{item.projectName}</h3>
                   <p> number of products: {item.products.length}</p>
 
@@ -231,41 +117,21 @@ const ProjectList = () => {
                       });
                     }}
                   >
-                    show report
+                    open map
                   </button>
-                </div>
+                  <button
+                    onClick={() => {
+                      handleGetReport(item, index);
+                    }}
+                  >
+                    get report
+                  </button>
+                </ProjectItem>
               );
             })}
-          </OldProjectContainer>
-        )}
-        <ActiveProjectContainer>
-          {activeProjectList.map((item, index) => {
-            return (
-              <ProjectItem key={index}>
-                <h3>{item.projectName}</h3>
-                <p> number of products: {item.products.length}</p>
-
-                <button
-                  onClick={() => {
-                    navigate("/user/project-detail", {
-                      state: { projectInfo: item },
-                    });
-                  }}
-                >
-                  open map
-                </button>
-                <button
-                  onClick={() => {
-                    handleGetReport(item, index);
-                  }}
-                >
-                  get report
-                </button>
-              </ProjectItem>
-            );
-          })}
-        </ActiveProjectContainer>
-      </ProjectListContainer>
+          </ActiveProjectContainer>
+        </ProjectListContainer>
+      )}
     </div>
   );
 };
--- src/components/UserPage/project/ProjectList/style.js
+++ src/components/UserPage/project/ProjectList/style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 const ProjectListContainer = styled.div`
   text-align: center;
   padding: 0.5rem;
@@ -8,10 +8,8 @@ const ActiveProjectContainer = styled.div`
   grid-template-columns: repeat(3, 1fr);
   gap: 1rem;
 
-  border-style: solid;
-  border-color: green;
-  padding-bottom: 10px;
-  margin-bottom: 5vh;
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
   border-radius: 5px;
 `;
 const ProjectItem = styled.div`
@@ -21,16 +19,42 @@ const OldProjectContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 1rem;
-  border-style: dashed;
-  border-color: black;
-  padding-bottom: 10px;
-  margin-bottom: 5vh;
+
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
   border-radius: 5px;
   background-color: lightgray;
 `;
+const spinAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+`;
+
+const Loader = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 5px solid #ccc;
+  border-top-color: #333;
+  border-radius: 50%;
+  animation: ${spinAnimation} 1s linear infinite;
+`;
+
 export {
   ActiveProjectContainer,
   ProjectListContainer,
   OldProjectContainer,
   ProjectItem,
+  LoaderContainer,
+  Loader,
 };
